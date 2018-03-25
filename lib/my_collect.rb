0602c1f3ef14919dbaf88a20f90dbbee77ad46ff
Benjamin Bowes <bbowes999@gmail.com>
2018-03-25 19:58:07 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,7 +5,17 @@ while i < collection.length
 
 empty_array << yield(collection[i])
 i += 1
+<<<<<<< HEAD
  end
 empty_array
 end
 
+=======
+end
+collection
+end
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+ my_collect(languages) do |language|
+      language.upcase
+end
+>>>>>>> a6f58136a7ca8623480fc6e80416457c338c9d25
